2fbb49a848b4a138941567751ca221ec2bfaa76c
bruce-dunwiddie <shriop@hotmail.com>
2016-06-27 22:54:23 -0500

    implementing select statment logic

--- TSQL_Parser/TSQL_Parser/Statements/Builders/TSQLSelectStatementBuilder.cs
+++ TSQL_Parser/TSQL_Parser/Statements/Builders/TSQLSelectStatementBuilder.cs
@@ -24,22 +24,262 @@ namespace TSQL.Statements.Builders
 
 			select.Tokens.Add(keyword);
 
+			// should whitespace be excluded from statement parsing logic?
+
+			// assert SELECT
+
+			// SELECT clause
+
+			// can contain ALL, DISTINCT, TOP, PERCENT, WITH TIES
+
+			// recursively walk down and back up parens
+
+			// ends with FROM, semicolon, or keyword other than those listed above, when used outside of parens
+
+			// should I differentiate keywords that start commands?
+
+			// correlated subqueries
+			// scalar function calls
+			int nestedLevel = 0;
+
 			while (
 				tokenizer.Read() &&
 				!(
-					tokenizer.Current is TSQLCharacter &&
+					tokenizer.Current.Type == TSQLTokenType.Character &&
 					tokenizer.Current.AsCharacter.Character == TSQLCharacters.Semicolon
+				) &&
+				(
+					nestedLevel > 0 ||
+					!(
+						tokenizer.Current.Type == TSQLTokenType.Keyword &&
+						(
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.INTO ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.FROM ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.WHERE ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.GROUP ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.HAVING ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.UNION ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.EXCEPT ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.INTERSECT ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.ORDER ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.FOR ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.OPTION
+						)
+					)
 				))
 			{
 				select.Tokens.Add(tokenizer.Current);
+
+				if (tokenizer.Current.Type == TSQLTokenType.Character)
+				{
+					TSQLCharacters character = tokenizer.Current.AsCharacter.Character;
+
+					if (character == TSQLCharacters.OpenParentheses)
+					{
+						// should we recurse for correlated subqueries?
+						nestedLevel++;
+					}
+					else if (character == TSQLCharacters.CloseParentheses)
+					{
+						nestedLevel--;
+					}
+				}
 			}
 
 			if (
-				tokenizer.Current is TSQLCharacter &&
+				tokenizer.Current.Type == TSQLTokenType.Keyword &&
+				tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.INTO
+			)
+			{
+				while (
+					tokenizer.Read() &&
+					!(
+						tokenizer.Current.Type == TSQLTokenType.Character &&
+						tokenizer.Current.AsCharacter.Character == TSQLCharacters.Semicolon
+					) &&
+					!(
+						tokenizer.Current.Type == TSQLTokenType.Keyword &&
+						(
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.FROM ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.WHERE ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.GROUP ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.HAVING ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.UNION ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.EXCEPT ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.INTERSECT ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.ORDER ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.FOR ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.OPTION
+						)
+					))
+				{
+					select.Tokens.Add(tokenizer.Current);
+				}
+			}
+
+
+			if (
+				tokenizer.Current.Type == TSQLTokenType.Keyword &&
+				tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.FROM
+			)
+			{
+				// derived tables
+				// TVF
+				nestedLevel = 0;
+
+				while (
+					tokenizer.Read() &&
+					!(
+						tokenizer.Current.Type == TSQLTokenType.Character &&
+						tokenizer.Current.AsCharacter.Character == TSQLCharacters.Semicolon
+					) &&
+					(
+						nestedLevel > 0 ||
+						!(
+							tokenizer.Current.Type == TSQLTokenType.Keyword &&
+							(
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.WHERE ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.GROUP ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.HAVING ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.UNION ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.EXCEPT ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.INTERSECT ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.ORDER ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.FOR ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.OPTION
+							)
+						)
+					))
+				{
+					select.Tokens.Add(tokenizer.Current);
+
+					if (tokenizer.Current.Type == TSQLTokenType.Character)
+					{
+						TSQLCharacters character = tokenizer.Current.AsCharacter.Character;
+
+						if (character == TSQLCharacters.OpenParentheses)
+						{
+							// should we recurse for derived tables?
+							nestedLevel++;
+						}
+						else if (character == TSQLCharacters.CloseParentheses)
+						{
+							nestedLevel--;
+						}
+					}
+				}
+			}
+
+			if (
+				tokenizer.Current.Type == TSQLTokenType.Keyword &&
+				tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.WHERE
+			)
+			{
+				// subqueries
+				nestedLevel = 0;
+
+				while (
+					tokenizer.Read() &&
+					!(
+						tokenizer.Current.Type == TSQLTokenType.Character &&
+						tokenizer.Current.AsCharacter.Character == TSQLCharacters.Semicolon
+					) &&
+					(
+						nestedLevel > 0 ||
+						!(
+							tokenizer.Current.Type == TSQLTokenType.Keyword &&
+							(
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.GROUP ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.HAVING ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.UNION ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.EXCEPT ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.INTERSECT ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.ORDER ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.FOR ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.OPTION
+							)
+						)
+					))
+				{
+					select.Tokens.Add(tokenizer.Current);
+
+					if (tokenizer.Current.Type == TSQLTokenType.Character)
+					{
+						TSQLCharacters character = tokenizer.Current.AsCharacter.Character;
+
+
+						if (character == TSQLCharacters.OpenParentheses)
+						{
+							// should we recurse for subqueries?
+							nestedLevel++;
+						}
+						else if (character == TSQLCharacters.CloseParentheses)
+						{
+							nestedLevel--;
+						}
+					}
+				}
+			}
+
+			if (
+				tokenizer.Current.Type == TSQLTokenType.Keyword &&
+				tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.GROUP
+			)
+			{
+				// subqueries
+				nestedLevel = 0;
+
+				while (
+					tokenizer.Read() &&
+					!(
+						tokenizer.Current.Type == TSQLTokenType.Character &&
+						tokenizer.Current.AsCharacter.Character == TSQLCharacters.Semicolon
+					) &&
+					(
+						nestedLevel > 0 ||
+						!(
+							tokenizer.Current.Type == TSQLTokenType.Keyword &&
+							(
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.HAVING ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.UNION ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.EXCEPT ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.INTERSECT ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.ORDER ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.FOR ||
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.OPTION
+							)
+						)
+					))
+				{
+					select.Tokens.Add(tokenizer.Current);
+
+					if (tokenizer.Current.Type == TSQLTokenType.Character)
+					{
+						TSQLCharacters character = tokenizer.Current.AsCharacter.Character;
+
+						if (character == TSQLCharacters.OpenParentheses)
+						{
+							// should we recurse for subqueries?
+							nestedLevel++;
+						}
+						else if (character == TSQLCharacters.CloseParentheses)
+						{
+							nestedLevel--;
+						}
+					}
+				}
+			}
+
+			if (
+				tokenizer.Current.Type == TSQLTokenType.Character &&
 				tokenizer.Current.AsCharacter.Character == TSQLCharacters.Semicolon
 			)
 			{
 				select.Tokens.Add(tokenizer.Current);
+			}
+			else if (tokenizer.Current != null)
+			{
+				tokenizer.Putback();
 			}
 
 			return select;
