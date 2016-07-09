89f6526bb30554ff59a64bd3e90e5ed569930e4a
Bruce Dunwiddie <shriop@hotmail.com>
2016-07-09 01:35:02 -0500

    adding clause parsers that didn't get commited

---
+++ TSQL_Parser/TSQL_Parser/Clauses/Parsers/TSQLIntoClauseParser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TSQL.Tokens;
+
+namespace TSQL.Clauses.Parsers
+{
+	public class TSQLIntoClauseParser : ITSQLClauseParser
+	{
+		public TSQLIntoClause Parse(TSQLTokenizer tokenizer)
+		{
+			TSQLIntoClause into = new TSQLIntoClause();
+
+			while (
+				tokenizer.Read() &&
+				!(
+					tokenizer.Current is TSQLCharacter &&
+					tokenizer.Current.AsCharacter.Character == TSQLCharacters.Semicolon
+				) &&
+				!(
+					tokenizer.Current.Type == TSQLTokenType.Keyword &&
+					(
+						tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.FROM ||
+						tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.UNION ||
+						tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.EXCEPT ||
+						tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.INTERSECT ||
+						tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.ORDER ||
+						tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.FOR ||
+						tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.OPTION
+					)
+				))
+			{
+				into.Tokens.Add(tokenizer.Current);
+			}
+
+			return into;
+		}
+
+		TSQLClause ITSQLClauseParser.Parse(TSQLTokenizer tokenizer)
+		{
+			return Parse(tokenizer);
+		}
+	}
+}
---
+++ TSQL_Parser/TSQL_Parser/Clauses/Parsers/TSQLOrderByClauseParser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TSQL.Tokens;
+
+namespace TSQL.Clauses.Parsers
+{
+	public class TSQLOrderByClauseParser : ITSQLClauseParser
+	{
+		public TSQLOrderByClause Parse(TSQLTokenizer tokenizer)
+		{
+			TSQLOrderByClause orderBy = new TSQLOrderByClause();
+
+			// subqueries
+			int nestedLevel = 0;
+
+			while (
+				tokenizer.Read() &&
+				!(
+					tokenizer.Current.Type == TSQLTokenType.Character &&
+					tokenizer.Current.AsCharacter.Character == TSQLCharacters.Semicolon
+				) &&
+				(
+					nestedLevel > 0 ||
+					!(
+						tokenizer.Current.Type == TSQLTokenType.Keyword &&
+						(
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.FOR ||
+							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.OPTION
+						)
+					)
+				))
+			{
+				orderBy.Tokens.Add(tokenizer.Current);
+
+				if (tokenizer.Current.Type == TSQLTokenType.Character)
+				{
+					TSQLCharacters character = tokenizer.Current.AsCharacter.Character;
+
+					if (character == TSQLCharacters.OpenParentheses)
+					{
+						// should we recurse for subqueries?
+						nestedLevel++;
+					}
+					else if (character == TSQLCharacters.CloseParentheses)
+					{
+						nestedLevel--;
+					}
+				}
+			}
+
+			return orderBy;
+		}
+
+		TSQLClause ITSQLClauseParser.Parse(TSQLTokenizer tokenizer)
+		{
+			return Parse(tokenizer);
+		}
+	}
+}
