bc6d466d39c599a64cf29c05fb68ea77c0cb0b09
Bruce Dunwiddie <shriop@hotmail.com>
2016-07-11 16:25:20 -0500

    tightened up where clause parsing logic

--- TSQL_Parser/TSQL_Parser/Clauses/Parsers/TSQLWhereClauseParser.cs
+++ TSQL_Parser/TSQL_Parser/Clauses/Parsers/TSQLWhereClauseParser.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using TSQL.Statements;
+using TSQL.Statements.Parsers;
 using TSQL.Tokens;
 
 namespace TSQL.Clauses.Parsers
@@ -33,19 +35,31 @@ namespace TSQL.Clauses.Parsers
 					tokenizer.Current.Type == TSQLTokenType.Character &&
 					tokenizer.Current.AsCharacter.Character == TSQLCharacters.Semicolon
 				) &&
+				!(
+					nestedLevel == 0 &&
+					tokenizer.Current.Type == TSQLTokenType.Character &&
+					tokenizer.Current.AsCharacter.Character == TSQLCharacters.CloseParentheses
+				) &&
 				(
 					nestedLevel > 0 ||
-					!(
+					tokenizer.Current.Type != TSQLTokenType.Keyword ||
+					(
 						tokenizer.Current.Type == TSQLTokenType.Keyword &&
+						tokenizer.Current.AsKeyword.Keyword.In
 						(
-							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.GROUP ||
-							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.HAVING ||
-							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.UNION ||
-							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.EXCEPT ||
-							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.INTERSECT ||
-							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.ORDER ||
-							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.FOR ||
-							tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.OPTION
+							TSQLKeywords.NULL,
+							TSQLKeywords.CASE,
+							TSQLKeywords.WHEN,
+							TSQLKeywords.THEN,
+							TSQLKeywords.ELSE,
+							TSQLKeywords.AND,
+							TSQLKeywords.OR,
+							TSQLKeywords.BETWEEN,
+							TSQLKeywords.EXISTS,
+							TSQLKeywords.END,
+							TSQLKeywords.IN,
+							TSQLKeywords.IS,
+							TSQLKeywords.NOT
 						)
 					)
 				))
@@ -56,11 +70,35 @@ namespace TSQL.Clauses.Parsers
 				{
 					TSQLCharacters character = tokenizer.Current.AsCharacter.Character;
 
-
 					if (character == TSQLCharacters.OpenParentheses)
 					{
 						// should we recurse for subqueries?
 						nestedLevel++;
+
+						if (tokenizer.Read())
+						{
+							if (
+								tokenizer.Current.Type == TSQLTokenType.Keyword &&
+								tokenizer.Current.AsKeyword.Keyword == TSQLKeywords.SELECT)
+							{
+								TSQLSelectStatement selectStatement = new TSQLSelectStatementParser().Parse(tokenizer);
+
+								where.Tokens.AddRange(selectStatement.Tokens);
+
+								if (
+									tokenizer.Current != null &&
+									tokenizer.Current.Type == TSQLTokenType.Character &&
+									tokenizer.Current.AsCharacter.Character == TSQLCharacters.CloseParentheses)
+								{
+									nestedLevel--;
+									where.Tokens.Add(tokenizer.Current);
+								}
+							}
+							else
+							{
+								where.Tokens.Add(tokenizer.Current);
+							}
+						}
 					}
 					else if (character == TSQLCharacters.CloseParentheses)
 					{
