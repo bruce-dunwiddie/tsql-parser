5734b4fd830f994a4a6dc996cfc09c46d546465f
bruce-dunwiddie <shriop@hotmail.com>
2016-09-03 21:19:48 -0500

    added placeholders for merge statement

---
+++ TSQL_Parser/TSQL_Parser/Statements/Parsers/TSQLMergeStatementParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSQL.Statements.Parsers
+{
+	public class TSQLMergeStatementParser : ITSQLStatementParser
+	{
+		TSQLMergeStatement Parse(TSQLTokenizer tokenizer)
+		{
+			throw new NotImplementedException();
+		}
+
+		TSQLStatement ITSQLStatementParser.Parse(TSQLTokenizer tokenizer)
+		{
+			return Parse(tokenizer);
+		}
+	}
+}
