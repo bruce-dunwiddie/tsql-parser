059cfdbd7eaa6d548d47e0ec966ffb0b4fea9087
bruce-dunwiddie <shriop@hotmail.com>
2018-07-16 00:12:50 -0500

    removing push to coveralls in manual code coverage script

--- TSQL_Parser/Tests/CodeCoverage.bat
+++ TSQL_Parser/Tests/CodeCoverage.bat
@@ -1,4 +1,4 @@
 "..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" -target:"..\packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe" -targetargs:"bin\Debug\Tests.dll"  -filter:"+[TSQL*]*" -excludebyattribute:"System.CodeDom.Compiler.GeneratedCodeAttribute" -register:user -output:"bin\Debug\CodeCoverageResult.xml"
 "..\packages\ReportGenerator.3.1.2\tools\ReportGenerator.exe" "-reports:bin\Debug\CodeCoverageResult.xml" "-targetdir:bin\Debug\CodeCoverageReport"
-"..\packages\coveralls.io.1.4.2\tools\coveralls.net.exe" --opencover "bin\Debug\CodeCoverageResult.xml" -r %COVERALLS_REPO_TOKEN%
+rem "..\packages\coveralls.io.1.4.2\tools\coveralls.net.exe" --opencover "bin\Debug\CodeCoverageResult.xml" -r %COVERALLS_REPO_TOKEN%
 pause
