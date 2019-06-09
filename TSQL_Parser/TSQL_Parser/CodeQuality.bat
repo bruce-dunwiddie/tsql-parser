a7b7c166dbcfeb5d2e0e53694ab8ad2bf7e66df5
Bruce Dunwiddie <shriop@hotmail.com>
2019-06-08 22:26:55 -0500

    Updated nuspec for latest release

--- TSQL_Parser/TSQL_Parser/CodeQuality.bat
+++ TSQL_Parser/TSQL_Parser/CodeQuality.bat
@@ -1,3 +1,5 @@
+rem run Tests/CodeCoverage.bat first
+
 SonarScanner.MSBuild.exe begin /k:"tsqlparser" /d:sonar.organization="bruce-dunwiddie-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login=%SONARQUBE_TOKEN% /d:sonar.cs.opencover.reportsPaths="..\Tests\bin\Debug\CodeCoverageResult.xml"
 
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MsBuild.exe" ..\TSQL_Parser.sln /t:Rebuild
