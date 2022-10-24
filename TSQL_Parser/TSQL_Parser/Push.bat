b76cea9b6cc02900cfad731d28baec7b970e4735
Bruce Dunwiddie <shriop@hotmail.com>
2022-10-23 19:43:27 -0500

    Updated version number for release.

--- TSQL_Parser/TSQL_Parser/Push.bat
+++ TSQL_Parser/TSQL_Parser/Push.bat
@@ -1,4 +1,4 @@
 nuget SetApiKey %NUGET_KEY%
-nuget push TSQL.Parser.2.5.0.snupkg -Source https://api.nuget.org/v3/index.json
-nuget push TSQL.Parser.2.5.0.nupkg -Source https://api.nuget.org/v3/index.json
+nuget push TSQL.Parser.2.6.0.nupkg -Source https://api.nuget.org/v3/index.json
+nuget push TSQL.Parser.2.6.0.snupkg -Source https://api.nuget.org/v3/index.json
 pause
